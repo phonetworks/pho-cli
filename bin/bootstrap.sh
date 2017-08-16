cf9cb941b61e0b43c3b8b7c09fb6e6606572a87a
Emre Sokullu <esokullu@gmail.com>
2017-08-16 09:34:14 -0700

    Update bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -6,9 +6,10 @@ sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
 sudo apt-get remove php7.0 -y
 sudo apt-get install -y curl php7.1 php7.1-fpm php7.1-cli php7.1-common php7.1-mbstring php7.1-gd php7.1-intl php7.1-xml php7.1-mysql php7.1-mcrypt php7.1-zip
+sudo apt-get install -y composer
 sudo dpkg -i /opt/pho-cli/bin/ubuntu-16.04/libgraphqlparser_0.6.0-0ubuntu1_amd64.deb
 sudo dpkg -i /opt/pho-cli/bin/ubuntu-16.04/php-graphql_0.6.0-0ubuntu1_amd64.deb
 cd ~
-sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+# sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 cd /opt/pho-cli
 composer install
