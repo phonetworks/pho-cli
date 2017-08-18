f2a0e964ad64dcbd64da12ec37ac0e03b9e5ad3f
Groups <esokullu@gmail.com>
2017-08-17 18:15:37 -0700

    improvements with the Vagrant

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 sudo apt-get update
 
-# Install php7.1 repository
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
 sudo apt-get remove php7.0 -y
 sudo apt-get install -y curl php7.1 php7.1-fpm php7.1-cli php7.1-common php7.1-mbstring php7.1-gd php7.1-intl php7.1-xml php7.1-mysql php7.1-mcrypt php7.1-zip
-sudo apt-get install -y composer
 sudo dpkg -i /opt/pho-cli/bin/ubuntu-16.04/libgraphqlparser_0.6.0-0ubuntu1_amd64.deb
 sudo dpkg -i /opt/pho-cli/bin/ubuntu-16.04/php-graphql_0.6.0-0ubuntu1_amd64.deb
 cd ~
 # sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 cd /opt/pho-cli
 composer install
+sudo mv /etc/php/7.0/mods-available/z_graphql.ini /etc/php/7.1/mods-available/z_graphql.ini
+sudo ln -s /etc/php/7.1/mods-available/z_graphql.ini /etc/php/7.1/cli/conf.d/21-graphql.ini
+sudo ln -s /usr/lib/php/20151012/graphql.so /usr/lib/php/20160303/graphql.so
+cd /tmp
+git clone https://github.com/dosten/graphql-parser-php
+cd graphql-parser-php
+phpize
+sed -i 's/graphql_parse_string/graphql_parse_string_with_experimental_schema_support/g' graphql.c
+./configure
+make && sudo make install
