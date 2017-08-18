8e208e9324b6fb01523debf9d269b476215a9711
Groups <esokullu@gmail.com>
2017-08-17 19:02:40 -0700

    updates to bootstrap script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,8 +4,8 @@ sudo apt-get update
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
 sudo apt-get remove php7.0 -y
-sudo apt-get install -y redis-server
-sudo apt-get install -y curl php7.1 php7.1-fpm php7.1-cli php7.1-common php7.1-mbstring php7.1-gd php7.1-intl php7.1-xml php7.1-mysql php7.1-mcrypt php7.1-zip
+sudo apt-get install -y redis-server gcc 
+sudo apt-get install -y curl php7.1 php7.1-fpm php7.1-cli php7.1-common php7.1-mbstring php7.1-gd php7.1-intl php7.1-xml php7.1-mysql php7.1-mcrypt php7.1-zip php7.1-dev
 sudo dpkg -i /opt/pho-cli/bin/ubuntu-16.04/libgraphqlparser_0.6.0-0ubuntu1_amd64.deb
 sudo dpkg -i /opt/pho-cli/bin/ubuntu-16.04/php-graphql_0.6.0-0ubuntu1_amd64.deb
 cd ~
@@ -18,7 +18,7 @@ sudo ln -s /usr/lib/php/20151012/graphql.so /usr/lib/php/20160303/graphql.so
 cd /tmp
 git clone https://github.com/dosten/graphql-parser-php
 cd graphql-parser-php
-phpize
-sed -i 's/graphql_parse_string/graphql_parse_string_with_experimental_schema_support/g' graphql.c
-./configure
-make && sudo make install
+sudo phpize
+sudo sed -i 's/graphql_parse_string/graphql_parse_string_with_experimental_schema_support/g' graphql.c
+sudo ./configure
+sudo make && sudo make install
