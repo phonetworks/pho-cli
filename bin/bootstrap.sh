45cd2f6b2dbb34215d58b35f127b72b954481cdc
Emre Sokullu <esokullu@gmail.com>
2018-01-09 03:58:50 -0800

    including neo4j

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -21,3 +21,19 @@ sudo ./configure
 sudo make && sudo make install
 cd /opt/pho-cli
 composer install
+
+# see https://github.com/phonetworks/pho-kernel/blob/master/bin/bootstrap.sh
+## neo4j
+## http://www.exegetic.biz/blog/2016/09/installing-neo4j-ubuntu-16-04/
+## http://debian.neo4j.org/
+sudo apt install -y htop
+sudo apt install -y default-jre default-jre-headless
+wget --no-check-certificate -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb http://debian.neo4j.org/repo stable/' | sudo tee /etc/apt/sources.list.d/neo4j.list
+sudo apt update
+sudo apt install -y neo4j
+sudo rm /var/lib/neo4j/data/dbms/auth
+sudo service neo4j restart
+curl -H "Content-Type: application/json" -X POST -d '{"password":"password"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
+## todo
+## perhaps remove mysql?
