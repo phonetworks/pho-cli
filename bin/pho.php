d923647a00efdd35cd5332b414f4656ff733cd50
Emre Sokullu <esokullu@gmail.com>
2020-04-26 17:19:17 -0700

    minor changes with the version of pho.php

--- bin/pho.php
+++ bin/pho.php
@@ -6,7 +6,8 @@ require __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\Console\Application;
 use Pho\Cli;
 
-$application = new Application('Pho', '0.2');
+$version = file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . "VERSION");
+$application = new Application('Pho', $version);
 $application->add(new Cli\BuildCommand());
 $application->add(new Cli\InitCommand());
 //$application->add(new Cli\ServeCommand());
